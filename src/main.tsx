cd27909afbff89bc1d64184db0e0dd3389de886a
Lim Dong Hyeon <ilhai6@naver.com>
2024-10-23 17:06:27 +0900

    Fix: Route v6 문법으로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Routing from "./Routing.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import Details from "./routes/[id]/details.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/:id",
+    element: <Details />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Routing />
+    <RouterProvider router={router} />
   </StrictMode>
 );
